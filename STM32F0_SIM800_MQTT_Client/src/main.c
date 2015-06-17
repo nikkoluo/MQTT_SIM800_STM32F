15db07d90fa45bae89528995272f13e3b1d78ce8
Brandon <brandon2255p@gmail.com>
2015-06-17 22:59:55 +0200

    Initial commit

---
+++ STM32F0_SIM800_MQTT_Client/src/main.c
@@ -0,0 +1,22 @@
+/*
+**
+**                           Main.c
+**
+**
+**********************************************************************/
+/*
+   Last committed:     $Revision: 00 $
+   Last changed by:    $Author: $
+   Last changed date:  $Date:  $
+   ID:                 $Id:  $
+
+**********************************************************************/
+#include "stm32f0xx_conf.h"
+
+int main(void)
+{
+  while(1)
+  {
+  
+  }
+}
