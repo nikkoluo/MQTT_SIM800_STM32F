08d2829afc410b02d2cd4bd7f9bc4cd2828768ca
Brandon <brandon2255p@gmail.com>
2015-06-18 23:01:55 +0200

    InitUART

--- STM32F0_SIM800_MQTT_Client/src/main.c
+++ STM32F0_SIM800_MQTT_Client/src/main.c
@@ -20,3 +20,22 @@ int main(void)
   
   }
 }
+
+void init_USART(void)
+{
+	 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
+	 	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE);
+	 	GPIO_PinAFConfig(GPIOA, GPIO_PinSource9, GPIO_AF_1);
+	 	GPIO_PinAFConfig(GPIOA, GPIO_PinSource10, GPIO_AF_1);
+	 	GPIO_InitTypeDef GPIO_InitStruct
+	 	{
+	 		GPIO_Pin = GPIO_Pin_9|GPIO_Pin_10;
+	 		GPIO_Mode = GPIO_Mode_AF;
+	 		GPIO_Speed = GPIO_Speed_10MHz;
+	 		GPIO_OType = GPIO_OType_PP;
+	 		GPIO_PuPd = GPIO_PuPd_UP; 
+	 	}
+	 	GPIO_Init(GPIO_InitStruct);
+	 	
+
+}
